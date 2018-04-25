52c02558790e32d16915e86789bb5a0b1020ee7d
o.khomandiak <oleh.khomandiak@volia.com>
2018-04-25 18:02:33 +0300

    kom1

--- src/main/java/com/khomandiak/courseWork/App.java
+++ src/main/java/com/khomandiak/courseWork/App.java
@@ -1,54 +1,82 @@
 package com.khomandiak.courseWork;
 
-import java.io.*;
 
-
-import com.itextpdf.text.Rectangle;
-import com.itextpdf.text.pdf.PdfReader;
-import com.itextpdf.text.pdf.parser.*;
-
-
-
-import javax.swing.*;
-import javax.swing.filechooser.FileSystemView;
-
+import java.util.Arrays;
 
 public class App {
-    private static String fileName;
+    private static int[][] data = new int[][]{
+            {-2, 1, 2, 3, 4, 5, -3},
+            {1, -1, 7, 60, 220, 55, 0},
+            {2, 22, -1, 40, 80, 100, 0},
+            {3, 140, 150, -1, 14, 70, 0},
+            {4, 440, 120, 70, -1, 7, 0},
+            {5, 21, 17, 28, 90, -1, 0},
+            {-4, 0, 0, 0, 0, 0, -1}};
 
     public static void main(String[] args) {
-
-        JFileChooser jfc = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
-        jfc.setCurrentDirectory(new File("E:\\sts-bundle\\workspace\\courseWork\\target"));
-        int returnValue = jfc.showOpenDialog(null);
-
-
-        if (returnValue == JFileChooser.APPROVE_OPTION) {
-            File selectedFile = jfc.getSelectedFile();
-            fileName = selectedFile.getAbsolutePath();
+        int n = data.length-1;
+        System.out.println("Дано: ");
+        printData(data);
+        System.out.println("Етап 1:");
+        for(int i = 1; i < n; i++){
+            data[i][n] = getMin(n, data[i]);
         }
-        Rectangle rect = new Rectangle(60, 60, 700, 700);
-        RenderFilter filter = new RegionTextRenderFilter(rect);
-        try {
-            PdfReader reader = new PdfReader(fileName);
-            String pattern = "(?<=References|Bibliography|References and Notes|Literature cited|REFERENCES)";
-            String pdf = "";
-            TextExtractionStrategy strategy;
-            for (int i = 1; i <= reader.getNumberOfPages(); i++) {
-                strategy = new FilteredTextRenderListener(new LocationTextExtractionStrategy(), filter);
-                pdf += "\n" + (PdfTextExtractor.getTextFromPage(reader, i, strategy));
+        printData(data);
+        for(int i = 1; i < n; i++){
+            int a = data[i][n];
+            for(int j = 1; j<n;j++) {
+                data[i][j] = data[i][j] > 0 ? (data[i][j] - a) : data[i][j];
             }
-            String incompleteReferences = pdf.split(pattern)[1];
-            pattern = "(?>=Acknowledgement|Autobiographical|Table|Appendix|Exhibit|Notes|About the Authors|Actual transaction|PAUL|$)";
-            String references = incompleteReferences.split(pattern)[0];
-            System.out.println(references);
-            SimpleGUI gui = new SimpleGUI();
-            gui.setLabel(new JTextArea(references));
-            gui.setVisible(true);
-        }  catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
         }
+        printData(data);
+        for(int i = 1; i < n; i++){
+            int[] col = new int[n+1];
+            for(int j = 0; j <= n; j++){
+                col[j] = data[j][i];
+            }
+            data[n][i] = getMin(n, col);
+        }
+        printData(data);
+        for(int i = 1; i < n; i++){
+            int b = data[n][i];
+            for(int j = 1; j<n;j++) {
+                data[j][i] = data[j][i] > 0 ? (data[j][i] - b) : data[j][i];
+            }
+        }
+        printData(data);
+    }
+
+    private static int getMin(int n, int[] datum) {
+        int min = Integer.MAX_VALUE;
+        for(int j = 1; j<n; j++) {
+            int next = datum[j];
+            min = next >= 0 ? Math.min(min, next) : min;
+        }
+        return min;
+    }
+
+    private static void printData(int[][] data) {
+        System.out.println(" |————————————————————————|");
+        for (int[] row : data) {
+            for (int aData : row) {
+                System.out.print(" | ");
+                if (aData == -4)
+                    System.out.print(" B ");
+                else if (aData == -3)
+                    System.out.print(" A ");
+                else if (aData == -2)
+                    System.out.print(" n ");
+                else if (aData == -1)
+                    System.out.print("---");
+                else if (aData < 10)
+                    System.out.print(" " + aData + " ");
+                else if (aData < 100)
+                    System.out.print(aData + " ");
+                else
+                    System.out.print(aData);
+            }
+            System.out.println(" |\n |————————————————————————|");
+        }
+        System.out.println("\n");
     }
 }
